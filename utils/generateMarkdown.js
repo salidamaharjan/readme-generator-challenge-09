deb9ab7608263b711f09833a56bf2722f30e5eca
salidamaharjan <salidamaharjan@outlook.com>
2024-01-03 09:27:01 -0600

    added license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,17 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license === "MIT") {
+        return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+      } else if(license === "Apache"){
+        return "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)";
+      } else {
+        return "";
+      }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-    if (license === "MIT") {
-        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-      } else if(license === "Apache"){
-        return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-      } else {
-        return "";
-      }
+    
 }
 
 // TODO: Create a function that returns the license section of README
@@ -53,7 +55,7 @@ ${data.usage}
 ${data.credits}
 
 ## License
-${data.license}
+${renderLicenseBadge(data.license)}
 
 ## Questions
 You can reach me at   
@@ -81,7 +83,7 @@ ${data.usage}
 ${data.credits} 
 
 ## License
-${data.license}
+${renderLicenseBadge(data.license)}
 
 ## Questions
 You can reach me at  
