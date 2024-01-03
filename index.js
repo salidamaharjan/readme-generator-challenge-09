44a07a20269b08a05b7cb210e3f3148f6d1922d4
salidamaharjan <salidamaharjan@outlook.com>
2024-01-02 20:45:34 -0600

    adding code to ask series of question

--- index.js
+++ index.js
@@ -37,7 +37,11 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    const ask = await inquirer.prompt(
+        questions
+    );
+}
 
 // Function call to initialize app
 init();
