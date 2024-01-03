43c5b9b37b317185a1a442a8e49d939045e7d48b
salidamaharjan <salidamaharjan@outlook.com>
2024-01-02 21:14:57 -0600

    more question added

--- index.js
+++ index.js
@@ -24,6 +24,37 @@ const questions = [
     name: "learning",
     message: "What did you learn?",
   },
+  {
+    name: "content",
+    type: "confirm",
+    message: "Do you want to add table of content?",
+  },
+  {
+    name: "installation",
+    message: "Installation Guide for your app",
+  },
+  {
+    name: "usage",
+    message: "Usage for your app",
+  },
+  {
+    name: "credits",
+    message: "Credits to add",
+  },
+  {
+    name: "license",
+    type: "list",
+    message: "License to add",
+    choices: ["Apache", "MIT"],
+  },
+  {
+    name: "github",
+    message: "Enter your GitHub username to add",
+  },
+  {
+    name: "email",
+    message: "Enter email id to add as a contact",
+  },
 ];
 
 // TODO: Create a function to write README file
@@ -41,7 +72,15 @@ async function init() {
     const ask = await inquirer.prompt(
         questions
     );
-    writeToFile("readme.md", `## ${ask.title}` )
+    writeToFile("readme.md", `## ${ask.title}
+
+## Description
+* ${ask.motivation}
+* ${ask.purpose}
+* ${ask.solves}
+* ${ask.learning}
+
+`);
 }
 
 // Function call to initialize app
